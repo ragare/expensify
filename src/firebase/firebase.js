e9d8de5233889ccb72658f0dd0cedc5c580564d9
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2019-08-30 11:56:28 +0200

    fetching data

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -17,33 +17,79 @@ firebase.initializeApp(config);
 
 const database = firebase.database()
 
-database.ref().set({
-    name: 'Rafael Garcia',
-    age: 57,
-    stressLevel: 6,
-    job: {
-        title: 'Software developer',
-        company: 'Google'
-    },
-    location: {
-        city: 'Valencia',
-        country: 'Spain'
-    }
-}).then((data) => {
-    console.log('Data is saved: ', data)
-}).catch((error)=>{
-    console.log('This failed. ', error)
-})
 
-database.ref().update({
-    stressLevel: 9,
-    'job/company': 'Amazon'    ,
-    'location/city': 'Seattle'
-}).then((data) => {
-    console.log('Data update is saved: ', data)
-}).catch((error)=>{
-    console.log('This update failed. ', error)
-})
+const watcher = database.ref().on('value',
+    (snapshot) => {
+        const val = snapshot.val()
+        console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`)
+    },
+    (e) => {
+        console.log('error in subscription', e)
+    })
+
+
+// const watcher = database.ref().on('value',
+//     (snapshot) => {
+//         const val = snapshot.val()
+//         console.log('Val: ', val)
+//     },
+//     (e) => {
+//         console.log('error in subscription', e)
+//     })
+
+// setTimeout(() => {
+//     database.ref('age').set(28)
+// }, 1500)
+
+// setTimeout(() => {
+//     database.ref().off('value', watcher)
+// }, 3000)
+
+
+// setTimeout(() => {
+//     database.ref('age').set(30)
+// }, 5000)
+
+
+
+// database.ref()
+//     .once('value')
+//     .then((snapshot) => {
+//         const val = snapshot.val()
+//         console.log('Val: ', val)
+//     })
+//     .catch(e => {
+//         console.log('Error: ', e)
+//     })
+
+
+// database.ref().set({
+//     name: 'Rafael Garcia',
+//     age: 57,
+//     stressLevel: 6,
+//     job: {
+//         title: 'Software developer',
+//         company: 'Google'
+//     },
+//     location: {
+//         city: 'Valencia',
+//         country: 'Spain'
+//     }
+// }).then((data) => {
+//     console.log('Data is saved: ', data)
+// }).catch((error)=>{
+//     console.log('This failed. ', error)
+// })
+
+// database.ref().update({
+//     stressLevel: 9,
+//     'job/company': 'Amazon'    ,
+//     'location/city': 'Seattle'
+// }).then((data) => {
+//     console.log('Data update is saved: ', data)
+// }).catch((error)=>{
+//     console.log('This update failed. ', error)
+// })
 
 // database.ref().remove()
 //     .then(function () {

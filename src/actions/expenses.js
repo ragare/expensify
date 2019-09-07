77c413f48306ff44775fc5aa788942cee47e25ab
Rafael Garcia <dev.ragare@gmail.com>
2019-09-07 17:38:23 +0200

    Remove expense in firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,5 +1,6 @@
 import uuid from 'uuid'
 import database from '../firebase/firebase'
+import { doesNotReject } from 'assert';
 
 // ADD_EXPENSE
 export const addExpense = (expense) => ({
@@ -30,6 +31,17 @@ export const removeExpense = ({ id }) => ({
     type: 'REMOVE_EXPENSE',
     id
 })
+
+export const startRemoveExpense = ({ id }) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).remove()
+            .then(() => {
+                dispatch(removeExpense({ id }))
+            })
+    }
+}
+
+
 // EDIT_EXPENSE
 export const editExpense = (id, update) => ({
     type: 'EDIT_EXPENSE',
--- src/test/actions/expenses.test.js
+++ src/test/actions/expenses.test.js
@@ -1,4 +1,4 @@
-import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses } from '../../actions/expenses'
+import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses, startRemoveExpense } from '../../actions/expenses'
 import expenses from '../fixtures/expenses'
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
@@ -117,6 +117,25 @@ test('should fetch the expenses from firebase', (done) => {
                 expenses
             })
             done()
+        })
+
+})
+
+test('should remove expense from firebase', (done) => {
+    const store = createMockStore({})
+    const id = expenses[0].id
+    store.dispatch(startRemoveExpense({ id }))
+        .then(() => {
+            const actions = store.getActions()
+            expect(actions[0]).toEqual({
+                type: 'REMOVE_EXPENSE',
+                id
+            })
+            database.ref(`expenses/${id}`).once('value')
+                .then((snapshot) => {
+                    expect(snapshot.val()).toBeNull()
+                    done()
+                })
         })
 
 })

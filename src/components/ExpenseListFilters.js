74ae1a23a91037160fa5e0aa04c336c0cc35b4ca
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2019-09-11 17:33:24 +0200

    Add an Edit expense css done

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -36,11 +36,13 @@ export class ExpenseListFilters extends React.Component {
                         <input
                             type="text"
                             className="text-input"
+                            placeholder="Search expenses"
                             value={this.props.filters.text}
                             onChange={this.onTextChange} />
                     </div>
                     <div className="input-group__item">
                         <select
+                            className="select"
                             value={this.props.filters.sortBy}
                             onChange={this.onSortChange}>
                             <option value="date">Date</option>
